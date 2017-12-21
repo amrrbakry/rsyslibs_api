926febd485ab6992744c17f3138f98c5dcc4e667
Amr El-Bakry <amrrbakry17@gmail.com>
2017-12-22 01:53:34 +0200

    Update project dependency serializer

--- app/serializers/project_dependency_serializer.rb
+++ app/serializers/project_dependency_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectDependencySerializer < ActiveModel::Serializer
-  attributes :id, :name, :version
+  attributes :id, :name, :version, :github_url, :website, :owner_name
 
   has_many :system_libraries
 end
